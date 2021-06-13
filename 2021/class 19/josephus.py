6c6421b685f73bea7145c4d75725ab64bdf0dd8a
mjsong <mjsong070712@gmail.com>
2021-06-13 16:59:04 +0900

    class 19

---
+++ 2021/class 19/josephus.py
@@ -0,0 +1,63 @@
+MAX_L = 1001
+
+# push, pop, empty, size, front, back
+
+import sys
+
+
+class Queue:
+	def __init__(self):
+		self.L = [0 for i in range(MAX_L)]
+		self.front = 0
+		self.back = -1
+
+	def push(self, item):
+		self.back += 1
+		if self.back == MAX_L:
+			self.back = 0
+		self.L[self.back] = item
+	
+	def pop(self):
+		tmp = self.L[self.front]
+		self.front += 1
+		if self.front == MAX_L:
+			self.front = 0
+		return tmp
+	
+	def empty(self):
+		return self.back - self.front == -1
+	
+	def size(self):
+		if self.back >= self.front:
+			return (self.back-self.front+1)
+		if self.empty():
+			return 0
+
+		return (MAX_L+(self.back-self.front+1))
+
+	def ret_front(self):
+		if self.empty():
+			return -1
+		return self.L[self.front]
+
+
+	def ret_back(self):
+		if self.empty():
+			return -1
+		return self.L[self.back]
+
+
+N, K = input().split()
+N, K = int(N), int(K)
+ans = []
+Q = Queue()
+
+for i in range(N):
+	Q.push(i+1)
+
+while not Q.empty():
+	for i in range(K-1):
+		Q.push(Q.pop())
+	ans.append(Q.pop())
+
+print("<" + str(ans)[1:-1] + ">")
---
+++ 2021/class 19/printer.py
@@ -0,0 +1,76 @@
+MAX_L = 8
+
+# push, pop, empty, size, front, back
+
+import sys
+
+
+class Queue:
+	def __init__(self):
+		self.L = [0 for i in range(MAX_L)]
+		self.front = 0
+		self.back = -1
+
+
+	def push(self, item):
+		self.back += 1
+		if self.back == MAX_L:
+			self.back = 0
+		self.L[self.back] = item
+	
+	def pop(self):
+		tmp = self.L[self.front]
+		self.front += 1
+		if self.front == MAX_L:
+			self.front = 0
+		return tmp
+	
+	def empty(self):
+		return self.back - self.front == -1
+	
+	def size(self):
+		if self.back >= self.front:
+			return (self.back-self.front+1)
+		if self.empty():
+			return 0
+
+		return (MAX_L+(self.back-self.front+1))
+
+	def check_print(self):
+		i = self.front + 1
+		if i == MAX_L:
+			i = 0
+		while (self.back != MAX_L-1 and i != self.back+1) or (self.back == MAX_L-1 and i != 0):
+			if self.L[self.front][0] < self.L[i][0]:
+				self.push(self.pop())
+				return
+			if self.L[self.front][0] >= self.L[i][0]:
+				if i == self.back:
+					return (self.pop())
+					
+			
+			i += 1
+			if i == MAX_L:
+				i = 0
+
+
+Q = Queue()
+
+tc = int(input())
+L = []
+for i in range(tc):
+	Q.back = Q.front-1
+	N, M = input().split()
+	imp = input().split()
+
+	for j in range(len(imp)):
+		Q.push([int(imp[j]), j])
+		# Q.push({
+		# 	"value":int(imp[j]),
+		# 	"index":j
+		# 	})
+
+	while Q.size() >= 2:
+		L.append(Q.check_print())
+	L.append(Q.pop())
+	print(L)
